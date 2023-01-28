859dfb5a0afa5b733ca7f06e17838da4a744639d
Wassim Akachi <wassim@BytesAndCodes.com>
2023-01-28 07:53:53 +0100

    simple java-maven example project

---
+++ src/main/java/com/techeule/examples/avro/ExampleApplication.java
@@ -0,0 +1,7 @@
+package com.techeule.examples.avro;
+
+public class ExampleApplication {
+    public static void main(final String[] args) {
+        System.out.println("Hello Tech Eule \uD83E\uDD89");
+    }
+}
